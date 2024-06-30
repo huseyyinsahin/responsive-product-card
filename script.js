16de9fb65b65a378ead808ed7b00b01b824f81ed
huseyyinsahin <huseysahin19@outlook.com>
2024-06-30 22:37:18 +0300

    project update

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const button1 = document.getElementById("button1");
 const button2 = document.getElementById("button2");
 const button3 = document.getElementById("button3");
 const select = document.getElementById("select");
+const price = document.querySelector(".price");
 
 button1.addEventListener("click", () => {
   button1.classList.add("add-color");
@@ -22,4 +23,11 @@ button3.addEventListener("click", () => {
   button2.classList.remove("add-color");
   button3.classList.add("add-color");
   select.classList.add("select-show");
+});
+
+select.addEventListener("change", function () {
+  let selectValue = parseFloat(this.value);
+  let basePrice = 1789.99;
+  let totalPrice = basePrice * selectValue;
+  price.innerHTML = `<sup>$</sup>${totalPrice.toFixed(2)}`;
 });
